c2f81ea5251d5bf835bd355f71c64d454e1f8ba7
Ridwan Chowdhury <rchowdhury1212@gmail.com>
2021-03-25 23:20:01 -0400

    Updated UI look and added input checks and clearing of input fields

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,57 +1,58 @@
 from tkinter import *
+from tkinter import ttk
 #from PIL import Image, ImageTk
 
+darkred = "#B22222"
+textcolor = "white"
 
-
-window=Tk()
-window.configure(background="red")
+window = Tk()
+window.configure(background=darkred)
 
 #image1 = Image.open("</cs348Image.png>")
 #test = ImageTk.PhotoImage(image1)
 #label1.place(x=10, y=10)
 
 
-
-
-lbl=Label(window, text="Create Account", fg='white', bg='red', font=("Helvetica", 40))
+lbl = Label(window, text="Create Account", fg=textcolor,
+            bg=darkred, font=("Helvetica", 40))
 lbl.place(x=170, y=50)
 
 
-firstNameLabel=Label(window, text="First Name", fg='black', bg='red', font=("Helvetica", 15))
+firstNameLabel = Label(window, text="First Name",
+                       fg=textcolor, bg=darkred, font=("Helvetica", 15))
 firstNameLabel.place(x=150, y=150)
-firstName=Entry(window, fg='black', bg='red', bd=2)
+firstName = Entry(window, fg=textcolor, bg=darkred, bd=2)
 firstName.place(x=300, y=150)
 
-lastNameLabel=Label(window, text="Last Name", fg='black', bg='red', font=("Helvetica", 15))
+lastNameLabel = Label(window, text="Last Name", fg=textcolor,
+                      bg=darkred, font=("Helvetica", 15))
 lastNameLabel.place(x=150, y=200)
-lastName=Entry(window, fg='black', bg='red', bd=2)
+lastName = Entry(window, fg=textcolor, bg=darkred, bd=2)
 lastName.place(x=300, y=200)
 
-emailLabel=Label(window, text="Email", fg='black', bg='red', font=("Helvetica", 15))
+emailLabel = Label(window, text="Email", fg=textcolor,
+                   bg=darkred, font=("Helvetica", 15))
 emailLabel.place(x=150, y=250)
-email=Entry(window, fg='black', bg='red', bd=2)
+email = Entry(window, fg=textcolor, bg=darkred, bd=2)
 email.place(x=300, y=250)
 
-userNameLabel=Label(window, text="Username", fg='black', bg='red', font=("Helvetica", 15))
+userNameLabel = Label(window, text="Username", fg=textcolor,
+                      bg=darkred, font=("Helvetica", 15))
 userNameLabel.place(x=150, y=300)
-userName=Entry(window, fg='black', bg='red', bd=2)
+userName = Entry(window, fg=textcolor, bg=darkred, bd=2)
 userName.place(x=300, y=300)
 
-passwordLabel=Label(window, text="Password", fg='black', bg='red', font=("Helvetica", 15))
+passwordLabel = Label(window, text="Password", fg=textcolor,
+                      bg=darkred, font=("Helvetica", 15))
 passwordLabel.place(x=150, y=350)
-password=Entry(window, fg='black', bg='red', bd=2, show="*")
+password = Entry(window, fg=textcolor, bg=darkred, bd=2, show="*")
 password.place(x=300, y=350)
 
-confirmLabel=Label(window, text="Confirm Password", fg='black', bg='red', font=("Helvetica", 15))
+confirmLabel = Label(window, text="Confirm Password",
+                     fg=textcolor, bg=darkred, font=("Helvetica", 15))
 confirmLabel.place(x=150, y=400)
-confirm=Entry(window, fg='black', bg='red', bd=2, show="*")
+confirm = Entry(window, fg=textcolor, bg=darkred, bd=2, show="*")
 confirm.place(x=300, y=400)
-
-
-
-
-
-
 
 
 def create():
@@ -61,23 +62,30 @@ def create():
     user = userName.get()
     passwordText = password.get()
     confirmPassword = confirm.get()
-    
-    print(first + " " + last + " " + emailText + " " + user + " " + passwordText + " " + confirmPassword)
-    
-    
 
-btn=Button(window, text="Create account", fg='blue', bg='red', command=create)
+    if not first or not last or not emailText or not user or not passwordText or not confirmPassword:
+        print("Some field(s) are empty!")
+    elif passwordText != confirmPassword:
+        print("Passwords are not the same!")
+    else:
+        print(first + " " + last + " " + emailText + " " +
+              user + " " + passwordText + " " + confirmPassword)
+        clear_fields()
+
+
+def clear_fields():
+    firstName.delete(0, 'end')
+    lastName.delete(0, 'end')
+    email.delete(0, 'end')
+    userName.delete(0, 'end')
+    password.delete(0, 'end')
+    confirm.delete(0, 'end')
+
+
+btn = ttk.Button(window, text="Create account", command=create)
 btn.place(x=220, y=500)
-    
-    
-    
-
-
-
 
 
 window.title('WhatToWatch')
 window.geometry("600x600+10+10")
 window.mainloop()
-
-
